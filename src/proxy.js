28f55c4b5eb0ea372e5f6dca3a1e7669e8d8bec6
﻿Nafisha Nowshin Tisha <nafisatisha230@gmail.com>
2026-05-22 21:35:39 +0600

    add middleware for private routes

--- src/proxy.js
+++ src/proxy.js
@@ -1,22 +1,26 @@
+// middleware.js
 import { NextResponse } from 'next/server'
-import { auth } from './lib/auth'
-import { headers } from 'next/headers'
- 
 
-export async function proxy(request) {
-    const session= await auth.api.getSession({
-        headers:await headers()
-    })
-if(!session){
-     return NextResponse.redirect(new URL('/login', request.url))
+const protectedRoutes = ['/add-tutor', '/my-tutors', '/my-bookings']
+
+export async function middleware(request) {
+  const isProtected = protectedRoutes.some((route) =>
+    request.nextUrl.pathname.startsWith(route)
+  )
+
+  if (!isProtected) return NextResponse.next()
+
+  const sessionToken =
+    request.cookies.get('better-auth.session_token')?.value ||
+    request.cookies.get('__Secure-better-auth.session_token')?.value
+
+  if (!sessionToken) {
+    return NextResponse.redirect(new URL('/login', request.url))
+  }
+
+  return NextResponse.next()
 }
 
-
-}
-
-
- 
- 
 export const config = {
-  matcher:[ '/add-tutor','/tutors/:path'],
+  matcher: ['/add-tutor', '/my-tutors/:path*', '/my-bookings/:path*'],
 }
